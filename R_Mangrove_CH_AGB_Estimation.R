c2c6c1e05fa35819dd5c3ec94073defcc08bec91
Novia Arinda Pradisty <79441802+napradisty@users.noreply.github.com>
2025-10-16 09:15:05 +0200

    Update R_Mangrove_CH_AGB_Estimation.R

--- R_Mangrove_CH_AGB_Estimation.R
+++ R_Mangrove_CH_AGB_Estimation.R
@@ -8,7 +8,7 @@
 
 # The model development data (i.e. RF model data BP.tif) consists of 15-m spatial resolution canopy height (Height.q95) and aboveground biomass (AGB.Chave) layers as reference data,
 # complemented by multiple layers of multisource remote sensing data (from Sentinel-1 (S1), Sentinel-2 (S2), SAOCOM-1 (SAO) and Copernicus GLO-30 DEM).
-# Non-proprietary and non-public data and is available in Zenodo: (DOI: https://zenodo.org/records/17359993?preview=1). 
+# Non-proprietary and non-public data and is available in Zenodo: (DOI: 10.5281/zenodo.17359993). 
 # SAOCOM-1 data can generally be requested and accessed at the SAOCOM Catalog of the Argentinean National Commission on Space Activities (CONAE) (https://catalog.saocom.conae.gov.ar/catalog/). 
 # Sentinel-1 and Sentinel-2 data can be publicly accessed at Copernicus Data Space Ecosystem (CDSE) of the European Space Agency (ESA) (https://dataspace.copernicus.eu/).
 # BP = Budeng-Perancak; TA = Tahura Ngurah Rai
@@ -45,6 +45,9 @@ library(zen4R)        # Data repository access
 tidymodels_prefer()
 
 ##########################   DATA FOR MODEL   ###########################
+
+# Access data from Zenodo repository
+download_zenodo("10.5281/zenodo.17359993")
 
 # Read 15-m spatial resolution canopy height - aboveground biomass (CH - AGB) data and project to UTM zone 50S
 ch_agb <- terra::rast('your/path/CH_AGB_15m.tif') %>%  terra::project('EPSG:32750')
@@ -1328,5 +1331,6 @@ scatter3D(clipE@data$X,clipE@data$Y,clipE@data$Z,pch = 16,colkey = FALSE, main="
           col.grid = "gray50", xlab="UTM Easting (m)", ylab="UTM Northing (m)", zlab="Normalized height (m)")
 
 dev.off()
+
 
 
